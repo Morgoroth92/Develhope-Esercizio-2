e1219565997972df03f8cdeab284bee19e38c2d7
Claudio Vittorio Carta <claudio.vittorio.carta@gmail.com>
2023-07-18 13:12:58 +0200

    Initial commit

---
+++ src/PrintYourName.java
@@ -0,0 +1,7 @@
+public class PrintYourName {
+    public static void main(String[] args) {
+        String name = "Claudio";
+        String surname = "Carta";
+        System.out.print("My name is "+ name +" "+ surname);
+    }
+}
